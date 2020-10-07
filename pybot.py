cdf2856f636e8485d63fdc384f607fc3a70bcde0
Mikael <mikael@rosel.se>
2020-10-07 23:03:02 +0200

    ham

--- pybot.py
+++ pybot.py
@@ -20,8 +20,8 @@ fishdelay = 3
 use_roh = False
 ### spell then runeloop int ###
 #spell = ["encurso magni", 4] # HMM
-spell = ["encurso magni ignis", 10] # GFB
-#spell = ["encuro vita", 3] # UH
+spell = ["encurso magni ignis", 4] # GFB
+#spell = ["encuro vita", 4] # UH
 #spell = ["encurso vita virtus", 2] # SD
 #spell = ["encreo mas amplio", 2] # Explo
 
