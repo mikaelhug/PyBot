cb34d34c89955fcd9bcccf76f30bb54e8aa8efaa
Mikael <mikael@rosel.se>
2020-10-23 13:36:59 +0200

    d

--- house_search.py
+++ house_search.py
@@ -1,7 +1,8 @@
 import requests,time
-from datetime import date
+from datetime import datetime
+# import datetime
 
-def mediviahouses(city, status, beds):
+def mediviahouses(city, status, beds, size = 0):
     print("\nSearching for houses in "+city+".")
     url = "https://medivia.online/community/houses/legacy/%s/%s" % (city, status)
     
@@ -30,7 +31,7 @@ def mediviahouses(city, status, beds):
             house_urls.append(fullurl)
 
 
-    for hurl in house_urls[0:1]:
+    for hurl in house_urls:
         page = requests.get(hurl, {'User-Agent': 'Mozilla/5.0'}).text
         page = page.split('<div class="title">')[1]
         endsplit = '''    </div>
@@ -40,36 +41,52 @@ def mediviahouses(city, status, beds):
         name_server = page.split('</div>')[0]
         name = name_server.split(' on')[0]
         server = name_server.split('on ')[1]
+        hsize = page.split('The house has a size of <b>')[1].split('</b> square meters')[0]
         rent = page.split('The monthly rent is <b>')[1].split('</b>')[0]
-        beds = page.split('The house has <b>')[1].split('</b>')[0]
-
-        # print(page)
-
-        print(name)
-        print(server)
-        print(rent)
-        print(beds)
+        nrbeds = page.split('The house has <b>')[1].split('</b>')[0]
         
         if status == "rented":
-            last_pay = page.split('The rent has been paid until')[1].split(', ',1)[1].split('. <br>')[0]
-            last_pay = last_pay.rsplit(' ',2)[0]
-            last_pay = date.strptime(last_pay, '%b %d, %Y')
-            today = datetime.today().date()
+            try:
+                last_pay = page.split('The rent has been paid until')[1].split(', ',1)[1].split('. <br>')[0]
+                last_pay = datetime.strptime(last_pay, '%b %d, %Y %I:%M %p')
+            except:
+                last_pay = datetime.now()
+
+            today = datetime.now()
             if last_pay < today:
-                print("not payed rent")
-            print(last_pay)
-            print(datetime.today().date())
+                if int(nrbeds) >= beds:
+                    print(name+" has not payed rent since "+str(last_pay)+".\n"+hurl+"\nThis house has "+str(beds)+" or more beds and rent: "+rent+" gp.\n")
 
-        # page = page.split('</b>  door')[0]
-        # nrbeds = page.split('</b>')[0]
-        # # time.sleep(0.2)
+        if status == "available":
+            if int(nrbeds) >= beds:
+                print("This house has "+nrbeds+" beds: "+hurl)
 
-        # if int(nrbeds) >= beds:
-        #     print(hurl)
+            if int(hsize) >= size:
+                print("This house is "+hsize+" sqm large: "+hurl)
+                
 
 
-# mediviahouses("yehsha", "available", 3)
-mediviahouses("yehsha", "rented", 3)
+mediviahouses("mittenhoff", "available", 4, 200)
+mediviahouses("eschen", "available", 4, 200)
+mediviahouses("arak", "available", 4, 200)
+mediviahouses("thoris", "available", 4, 200)
+mediviahouses("osaris", "available", 4, 200)
+mediviahouses("garrogat", "available", 4, 200)
+mediviahouses("abukir", "available", 4, 200)
+mediviahouses("icenhaal", "available", 4, 200)
+mediviahouses("yehsha", "available", 4, 200)
+
+
+# mediviahouses("mittenhoff", "rented", 4)
+# mediviahouses("eschen", "rented", 4)
+# mediviahouses("arak", "rented", 4)
+# mediviahouses("thoris", "rented", 4)
+# mediviahouses("osaris", "rented", 4)
+# mediviahouses("garrogat", "rented", 4)
+# mediviahouses("abukir", "rented", 4)
+# mediviahouses("icenhaal", "rented", 4)
+# mediviahouses("yehsha", "rented", 4)
+
 
 # mediviahouses("mittenhoff")
 # mediviahouses("eschen")
